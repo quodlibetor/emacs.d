994c60241816689515ecc024ecf9c3942f0912bc
Brandon W Maister <quodlibetor@gmail.com>
2012-07-30 15:29:18 -0400

    Add a virtualenv yasnippet

---
+++ snippets/emacs-lisp-mode/virtualenv-dir-locals.el
@@ -0,0 +1,6 @@
+# -*- mode: snippet -*-
+# name: virtualenv dir locals
+# key: virtualenv
+# --
+((nil . ((virtualenv-workon . "$1")
+	 (virtualenv-default-directory . "${2:`default-directory`}"))))
