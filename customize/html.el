6769cd5f850b932ddb291212d6da44dcb33a0b4d
Brandon W Maister <quodlibetor@gmail.com>
2012-08-29 14:48:55 -0400

    Add html customizations, and zencoding-mode

---
+++ customize/html.el
@@ -0,0 +1,6 @@
+
+(add-hook 'html-mode-hook
+          (lambda ()
+            (zencoding-mode)
+            ;; seriously zencoding, you're stealing my C-j?
+            (define-key zencoding-mode-keymap (kbd "C-j") 'newline-and-indent)))
