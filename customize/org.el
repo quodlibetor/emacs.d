6eca43200cbf1a3c11a9cf3369b841b6af9872ce
Brandon W Maister <quodlibetor@gmail.com>
2012-09-19 13:22:54 -0400

    Make `org-agenda-files` dynamic
    Set them on each agenda load, instead of at emacs load

--- customize/org.el
+++ customize/org.el
@@ -1,5 +1,4 @@
 (setq org-hide-leading-stars t
-      org-agenda-files (append '("~/work.org" "~/life.org") (file-expand-wildcards "~/projects/*/*.org"))
       org-columns-default-format "%35ITEM(Task) %17Effort(Estimated Effort){:} %CLOCKSUM(Time Spent) %TODO %3PRIORITY %TAGS"
       org-clock-into-drawer t
       org-todo-keywords '((sequence "TODO(t)" "IN-PROGRESS(i)" "|" "DONE(d)")
@@ -9,6 +8,14 @@
       org-use-speed-commands t
       org-clock-out-remove-zero-time-clocks t
       )
+
+(defun set-agenda-files ()
+  (setq org-agenda-files (append '("~/work.org" "~/life.org") (file-expand-wildcards "~/projects/*/*.org")))
+  (message "Agenda files set to %s" org-agenda-files))
+
+(defadvice org-agenda (before refresh-agenda-files activate)
+  "make sure that all agenda files are included"
+  (set-agenda-files))
 
 (add-hook 'org-mode-hook
 	  (lambda ()
