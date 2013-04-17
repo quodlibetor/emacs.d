49c4b3396f9156d5de15d7cda1b352bcf38fbbed
Brandon W Maister <quodlibetor@gmail.com>
2013-04-17 14:18:46 -0400

    updated a bunch of packages

---
+++ elpa/httpcode-20120918.1347/httpcode-autoloads.el
@@ -0,0 +1,29 @@
+;;; httpcode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (hc) "httpcode" "httpcode.el" (20846 59209 597896
+;;;;;;  215000))
+;;; Generated autoloads from httpcode.el
+
+(autoload 'hc "httpcode" "\
+Display the meaning of an HTTP status code
+
+\(fn CODE)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("httpcode-pkg.el") (20846 59209 702482
+;;;;;;  462000))
+
+;;;***
+
+(provide 'httpcode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; httpcode-autoloads.el ends here
