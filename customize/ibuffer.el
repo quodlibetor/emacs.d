ff912e0e336235df2a1546d11d7e15ebaa1bd2c4
Brandon W Maister <quodlibetor@gmail.com>
2013-07-17 13:01:08 -0400

    Make filter groups include appnames

--- customize/ibuffer.el
+++ customize/ibuffer.el
@@ -1,13 +1,27 @@
+(eval-when-compile
+  (require 'cl))
+
+(defun bwm/list-app-dirs ()
+  (let ((app-dirs nil))
+    (dolist (code-dir (directory-files "~/projects" nil "^[^.]"))
+      (setq app-dirs
+            (cons `(,code-dir (filename . ,(concat (expand-file-name "~/projects") "/" code-dir)))
+                  app-dirs)))
+    app-dirs))
+
 (setq ibuffer-saved-filter-groups
-      '(("default"
-	 ("code" (mode . python-mode))
-	 ("wiki" (or
-		  (mode . confluence-edit-mode)
-		  (mode . confluence-mode)))
-	 ("notes" (mode . org-mode))
-	 ("emacs" (or
-		   (mode . emacs-lisp)
-		   (filename . "emacs.d/"))))))
+      (list
+       (cons "verbose"
+             (apply #'append
+                    (bwm/list-app-dirs)
+                    '((("code" (mode . python-mode))
+                       ("wiki" (or
+                                (mode . confluence-edit-mode)
+                                (mode . confluence-mode)))
+                       ("notes" (mode . org-mode))
+                       ("emacs" (or
+                                 (mode . emacs-lisp)
+                                 (filename . "emacs.d/")))))))))
 (add-hook 'ibuffer-mode-hook
 	  (lambda ()
-	    (ibuffer-switch-to-saved-filter-groups "default")))
+	    (ibuffer-switch-to-saved-filter-groups "verbose")))
