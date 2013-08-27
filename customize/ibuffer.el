f6a86c87220d84059bf66226373cd7d228c47053
Brandon W Maister <quodlibetor@gmail.com>
2013-08-27 09:20:34 -0400

    Make ibuffer filter groups reloadable

--- customize/ibuffer.el
+++ customize/ibuffer.el
@@ -7,31 +7,53 @@
       (setq app-dirs
             (cons `(,code-dir (filename . ,(concat (expand-file-name "~/projects") "/" code-dir)))
                   app-dirs)))
-    app-dirs))
+    (reverse app-dirs)))
 
-(setq ibuffer-saved-filter-groups
-      (list
-       (cons "verbose"
-             (apply #'append
-                    (bwm/list-app-dirs)
-                    '((("code" (mode . python-mode))
-                       ("wiki" (or
-                                (mode . confluence-edit-mode)
-                                (mode . confluence-mode)))
-                       ("notes" (mode . org-mode))
-                       ("emacs" (or
-                                 (mode . emacs-lisp)
-                                 (filename . "emacs.d/")))))))
-       '("simple"
-         ("code" (mode . python-mode))
-         ("wiki" (or
-                  (mode . confluence-edit-mode)
-                  (mode . confluence-mode)))
-         ("notes" (mode . org-mode))
-         ("emacs" (or
-                   (mode . emacs-lisp)
-                   (filename . "emacs.d/"))))))
+(setq ibuffer-show-empty-filter-groups nil)
+(defun bwm/reload-ibuffer-filter-groups ()
+  "Reload app-dirs"
+  (setq ibuffer-saved-filter-groups
+        (list
+         (cons "verbose"
+               (apply #'append
+                      '(("Notes" (mode . org-mode))
+                        ("Explore" (or (mode . compilation-mode)
+                                       (name . "*nosetests*")
+                                       (mode . grep-mode))))
+                      (bwm/list-app-dirs)
+                      '((("Code" (mode . python-mode))
+                         ("Wiki" (or (mode . confluence-edit-mode)
+                                     (mode . confluence-mode)))
+                         ("Emacs" (or (mode . emacs-lisp)
+                                      (filename . "emacs.d/")))
+                         ("VC" (or (mode . magit-status-mode)
+                                   (mode . magit-commit-mode)
+                                   (mode . magit-log-mode)
+                                   (mode . vc-annotate-mode)))
+                         ("Dired" (mode . dired-mode))))))
+         '("simple"
+           ("Code" (mode . python-mode))
+           ("Explore" (or (mode . compilation-mode)
+                          (name . "*nosetests*")
+                          (mode . grep-mode)))
+           ("Wiki" (or (mode . confluence-edit-mode)
+                       (mode . confluence-mode)))
+           ("Notes" (mode . org-mode))
+           ("Emacs" (or (mode . emacs-lisp)
+                        (filename . "emacs.d/")))
+           ("VC" (or (mode . magit-status-mode)
+                     (mode . magit-commit-mode)
+                     (mode . magit-log-mode)
+                     (mode . vc-annotate-mode)))
+           ("Dired" (mode . dired-mode))))))
+(bwm/reload-ibuffer-filter-groups)
+
+(defun bwm/ibuffer-update (arg &optional silent)
+  (interactive "P")
+  (bwm/reload-ibuffer-filter-groups)
+  (ibuffer-update arg silent))
 
 (add-hook 'ibuffer-mode-hook
 	  (lambda ()
-	    (ibuffer-switch-to-saved-filter-groups "verbose")))
+	    (ibuffer-switch-to-saved-filter-groups "verbose")
+            (local-set-key "g" 'bwm/ibuffer-update)))
