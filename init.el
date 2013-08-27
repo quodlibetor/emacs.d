5ef14f47104c22e36a300e84d51b3bc2f42d6395
Brandon W Maister <quodlibetor@gmail.com>
2013-08-27 09:22:29 -0400

    Add some theme/path customizations

--- init.el
+++ init.el
@@ -38,6 +38,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes (quote ("7b4a6cbd00303fc53c2d486dfdbe76543e1491118eba6adc349205dbf0f7063a" default))))
+ '(dired-omit-files "^\\.?#\\|^\\.")
+ '(exec-path (quote ("/home/bwm/.local/bin" "/home/bwm/bin" "/usr/lib/lightdm/lightdm" "/usr/local/sbin" "/usr/local/bin" "/usr/sbin" "/usr/bin" "/sbin" "/bin" "/usr/games" "/usr/local/games" "/home/bwm/.local/libexec/emacs/24.3/x86_64-unknown-linux-gnu")))
+ '(safe-local-variable-values (quote ((gud-pdb-command-name "ipdb") (virtualenv-default-directory . "/home/bwm/projects/pacman/") (virtualenv-workon . "pacman") (virtualenv-default-directory . "/home/bwm/projects/adi.proftools/") (virtualenv-workon . "adi.proftools") (virtualenv-default-directory . "/home/bwm/projects/incinerator/") (virtualenv-workon . "incinerator") (org-default-notes-file . "/home/bwm/projects/adi.proftools/adi.proftools.org") (org-default-notes-file . "/home/bwm/projects/subman/subman.org") (virtualenv-default-directory . "/home/bwm/projects/ingestor/") (virtualenv-workon . "ingestor") (org-default-notes-file . "/home/bwm/projects/ingestor/ingestor.org") (virtualenv-default-directory . "/home/bwm/projects/subman") (virtualenv-workon . "subman")))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -45,6 +48,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ediff-current-diff-B ((t (:background "#222" :foreground "DarkOrchid"))))
+ '(ediff-fine-diff-B ((t (:background "#023D2C"))))
+ '(font-lock-builtin-face ((t (:foreground "#ff83fa"))))
+ '(helm-selection ((t (:background "ForestGreen" :foreground "SlateGray1" :underline t))) t)
  '(magit-diff-add ((t (:inherit diff-added :foreground "forest green"))))
  '(magit-diff-del ((t (:inherit diff-removed :foreground "IndianRed1"))))
  '(magit-item-highlight ((t (:inherit highlight :background "#232323" :foreground "light sea green"))))
