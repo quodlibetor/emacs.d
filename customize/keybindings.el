7a28097305939c9a2e5ccff152bbbd072edaf917
Brandon W Maister <quodlibetor@gmail.com>
2014-01-16 13:49:53 -0500

    Add a keybinding for magit-blame-mode

--- customize/keybindings.el
+++ customize/keybindings.el
@@ -3,6 +3,7 @@
 (global-set-key (kbd "<f4>") 'flymake-goto-next-error)
 (global-set-key (kbd "C-c w") 'fixup-whitespace)
 (global-set-key (kbd "C-c g") 'magit-status)
+(global-set-key (kbd "C-c b") 'magit-blame-mode)
 (global-set-key (kbd "C-c f") 'helm-locate)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "M-/") 'hippie-expand)
