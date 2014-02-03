c8ccfc9095683a2cdabb13dc40e37b2e903aa80a
Brandon W Maister <quodlibetor@gmail.com>
2014-02-02 21:58:47 -0500

    Add byte-compile-file to elisp-mode keybindings

--- customize/elisp.el
+++ customize/elisp.el
@@ -1,3 +1,15 @@
+(defun bwm/byte-compile (arg)
+  "Either eval the current file, or recompile emacs.d
+
+Yeah I know they're not actually related functions."
+  (interactive "P")
+  (if arg
+      (let ((dir (expand-file-name "~/.emacs.d")))
+        (message "recompiling %s" dir)
+        (byte-recompile-directory dir 0))
+    (eval-buffer)))
+
 (add-hook 'emacs-lisp-mode-hook
 	  (lambda ()
-	    (paredit-mode)))
+            (paredit-mode)
+            (local-set-key (kbd "C-c C-c") 'bwm/byte-compile)))
