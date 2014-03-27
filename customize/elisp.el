b4410ce5924c8c9c7215f9c69010d67e1f2208f8
Brandon W Maister <quodlibetor@gmail.com>
2014-03-27 13:14:29 -0400

    Make bwm/byte-compile more better

--- customize/elisp.el
+++ customize/elisp.el
@@ -1,13 +1,17 @@
 (defun bwm/byte-compile (arg)
-  "Either eval the current file, or recompile emacs.d
+  "Either eval the current file, compile the file, or recompile emacs.d
 
 Yeah I know they're not actually related functions."
   (interactive "P")
-  (if arg
-      (let ((dir (expand-file-name "~/.emacs.d")))
-        (message "recompiling %s" dir)
-        (byte-recompile-directory dir 0))
-    (eval-buffer)))
+  (cond ((and arg (> 4 (car arg)))
+         (let ((dir (expand-file-name "~/.emacs.d")))
+           (message "recompiling %s" dir)
+           (byte-recompile-directory dir 0)))
+        (arg
+         (byte-compile-file (buffer-file-name)))
+        (t
+         (eval-buffer)
+         (message "eval'd %s" (buffer-file-name)))))
 
 (add-hook 'emacs-lisp-mode-hook
 	  (lambda ()
