0115051eb75bb9e646bd0f631bb81c70fed5aa51
Brandon W Maister <quodlibetor@gmail.com>
2014-04-02 17:29:34 -0400

    Add puppet mode because this is my life

---
+++ elpa/puppet-mode-20140325.944/puppet-mode-autoloads.el
@@ -0,0 +1,31 @@
+;;; puppet-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (puppet-mode) "puppet-mode" "puppet-mode.el" (21300
+;;;;;;  13827 630556 239000))
+;;; Generated autoloads from puppet-mode.el
+
+(autoload 'puppet-mode "puppet-mode" "\
+
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.pp\\'" . puppet-mode))
+
+;;;***
+
+;;;### (autoloads nil nil ("puppet-mode-pkg.el") (21300 13827 754160
+;;;;;;  501000))
+
+;;;***
+
+(provide 'puppet-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; puppet-mode-autoloads.el ends here
