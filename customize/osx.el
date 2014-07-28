a552973488533ddf61ea0db66e3518333ca2fd5c
Brandon W Maister <quodlibetor@gmail.com>
2014-07-28 16:55:44 -0400

    Command+w must not delete frames in OSX
    That is so painful. It's right where M-w is supposed to be!

--- customize/osx.el
+++ customize/osx.el
@@ -2,3 +2,20 @@
 (add-to-list 'exec-path "/usr/local/opt/coreutils/libexec/gnubin")
 (setq locate-command "mdfind")
 (setq helm-locate-command "mdfind %s %s")
+(global-set-key (kbd "s-w") 'kill-ring-save)
+(dolist (path '("/usr/local/Library/ENV/4.3"
+                "/usr/local/opt/autoconf/bin"
+                "/usr/local/opt/automake/bin"
+                "/usr/local/opt/pkg-config/bin"
+                "/usr/local/opt/libtasn1/bin"
+                "/usr/local/opt/nettle/bin"
+                "/usr/local/opt/gnutls/bin"
+                "/usr/local/opt/git/bin"
+                "/usr/bin"
+                "/bin"
+                "/usr/sbin"
+                "/sbin"
+                "/private/tmp/emacs-51yb/lib-src"
+                "/usr/local/Cellar/emacs/HEAD/libexec/emacs/24.4.50/x86_64-apple-darwin13.3.0"
+                "/usr/local/bin"))
+        (add-to-list 'exec-path path))
