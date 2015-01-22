9bbb898452364aafa95e9b2d0d498e6a0a04af2c
Brandon W Maister <quodlibetor@gmail.com>
2015-01-21 22:42:10 -0500

    Use repeat

--- customize/keybindings.el
+++ customize/keybindings.el
@@ -14,6 +14,7 @@
 (global-set-key (kbd "C-r") 'isearch-backward-regexp)
 (global-set-key (kbd "M-%") 'query-replace-regexp)
 (global-set-key (kbd "C-c O") 'rotate-windows)
+(global-set-key (kbd "C-z") 'repeat)
 (winner-mode 1) ;; enable "C-c →" and "C-c ←" to switch between previous window
                 ;; layouts
 (require 'iedit) ;; `C-;' means edit everything in buffer (or narrowed region)
