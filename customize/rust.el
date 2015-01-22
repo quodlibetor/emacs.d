08d40e6a9945aabbfd10b3957c4e946cafe86a7b
Brandon W Maister <quodlibetor@gmail.com>
2015-01-21 22:42:24 -0500

    Extra-fancy newline, going to use it all over

--- customize/rust.el
+++ customize/rust.el
@@ -1,4 +1,14 @@
 (require 'rust-mode)
 
-(define-key rust-mode-map (kbd "C-j") 'newline)
+(defun bwm:sane-newline (point)
+  "Newline and indent and indent closing brace"
+  (interactive "d")
+  (when (member (and (not (= point (point-max)))
+                     (buffer-substring point (+ point 1))) '("}" "]"))
+    (save-excursion (insert "\n")
+                    (forward-char)
+                    (indent-according-to-mode)))
+  (electric-newline-and-maybe-indent))
+
+(define-key rust-mode-map (kbd "C-j") 'bwm:sane-newline)
 (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
