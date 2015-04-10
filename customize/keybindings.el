1c42f31c55324ea618a0dbbc9485455a5d1adb13
Brandon W Maister <quodlibetor@gmail.com>
2015-04-10 13:08:16 -0400

    helm-apropos is better than regular apropos

--- customize/keybindings.el
+++ customize/keybindings.el
@@ -17,7 +17,9 @@
 (global-set-key (kbd "M-%") 'query-replace-regexp)
 (global-set-key (kbd "C-c O") 'rotate-windows)
 (global-set-key (kbd "C-c C-;") 'endless/comment-line-or-region)
+(global-set-key (kbd "C-h a") 'helm-apropos)
 (global-set-key (kbd "C-z") 'repeat)
+
 (winner-mode 1) ;; enable "C-c →" and "C-c ←" to switch between previous window
                 ;; layouts
 (require 'iedit) ;; `C-;' means edit everything in buffer (or narrowed region)
