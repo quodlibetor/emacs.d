357d24956a01dddfbe279773d17dda6cf154f057
Brandon W Maister <quodlibetor@gmail.com>
2015-06-07 09:48:41 -0400

    upgrade 15 packages

---
+++ elpa/ibuffer-tramp-20150527.1335/ibuffer-tramp-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ibuffer-tramp" "20150527.1335" "Group ibuffer's list by TRAMP connection" 'nil :url "http://github.com/svend/ibuffer-tramp" :keywords '("convenience"))
