c2fb7623a722d54afc0bd3f5d9dcfce9d5b9e898
Brandon W Maister <quodlibetor@gmail.com>
2015-06-27 11:25:39 -0400

    update magit

--- site-lisp/customize/magit.el
+++ site-lisp/customize/magit.el
@@ -2,7 +2,7 @@
 ;      magit-gitk-executable "/usr/bin/gitk")
 
 (setq magit-last-seen-setup-instructions "1.4.0")
-(add-to-list 'load-path "~/.emacs.d/site-lisp/magit")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/magit/lisp")
 (require 'magit)
 
 (define-key magit-mode-map "\t" 'magit-section-cycle)
