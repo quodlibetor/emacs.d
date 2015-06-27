55acf09d5c2981c02dac6d377c381b158c67e489
Brandon W Maister <quodlibetor@gmail.com>
2015-06-27 11:32:29 -0400

    Upgrade packages

--- elpa/highlight-20150405.2145/highlight-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "highlight" "20150405.2145" "Highlighting commands." 'nil :url "http://www.emacswiki.org/highlight.el" :keywords '("faces" "help" "local"))
