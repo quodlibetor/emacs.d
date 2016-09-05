567824b7dbeef1d6c18dea57cdf269ec84221ff5
Brandon W Maister <quodlibetor@gmail.com>
2016-09-05 15:10:07 -0400

    Magit commits should always be utf-8

--- site-lisp/customize/magit.el
+++ site-lisp/customize/magit.el
@@ -7,5 +7,9 @@
       magit-push-always-verify nil
       magit-visit-ref-create t)
 
+(add-hook 'magit-commit-mode-hook
+          (lambda ()
+            (setq buffer-file-coding-system 'utf-8)))
+
 (define-key magit-mode-map "\t" 'magit-section-cycle)
 (define-key magit-mode-map [C-tab] 'magit-section-toggle)
