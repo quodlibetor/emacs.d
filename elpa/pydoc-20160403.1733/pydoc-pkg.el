7b60c7a0d41314b9419f634fd8c211283cfa6d6e
Brandon W Maister <quodlibetor@gmail.com>
2016-09-05 15:07:36 -0400

    update some packages

---
+++ elpa/pydoc-20160403.1733/pydoc-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "pydoc" "20160403.1733" "functional, syntax highlighted pydoc navigation" 'nil :url "https://github.com/statmobile/pydoc" :keywords '("pydoc" "python"))
---
+++ elpa/thingatpt+-20151231.1610/thingatpt+-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "thingatpt+" "20151231.1610" "Extensions to `thingatpt.el'." 'nil :url "http://www.emacswiki.org/thingatpt%2b.el" :keywords '("extensions" "matching" "mouse"))
