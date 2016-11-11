c36f366d57384d983e9dec8bb2df1466353c1c79
Brandon W Maister <quodlibetor@gmail.com>
2016-11-11 10:27:53 -0500

    Slight improvements to rust autocomplete

--- site-lisp/customize/rust.el
+++ site-lisp/customize/rust.el
@@ -6,8 +6,12 @@
 (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
 (add-hook 'rust-mode-hook #'racer-mode)
 (add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'racer-mode-hook #'company-mode)
+(add-hook 'rust-mode-hook
+          (lambda ()
+            (setq-local company-minimum-prefix-length 0)))
+
 (setq racer-cmd (concat (getenv "HOME") "/.cargo/bin/racer"))
 (setq racer-rust-src-path (concat (getenv "HOME") "/src/rust/src"))
-(add-hook 'racer-mode-hook #'company-mode)
 (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
 (setq company-tooltip-align-annotations t)
