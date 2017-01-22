c5dc872239a431f4701b5872779627acc3a9a933
Brandon W Maister <quodlibetor@gmail.com>
2017-01-21 19:48:22 -0500

    make markdown mode line-wrap by default.

--- site-lisp/customize/markdown.el
+++ site-lisp/customize/markdown.el
@@ -2,3 +2,8 @@
 	     '("\\.md\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist
 	     '("github.com.*txt\\'" . markdown-mode))
+
+(add-hook 'markdown-mode-hook
+          (lambda ()
+            (visual-fill-column-mode 1)
+            (toggle-word-wrap 1)))
