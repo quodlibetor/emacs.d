ab0f5458f3def764cc94aae07d2a35477d387c0a
Brandon W Maister <quodlibetor@gmail.com>
2017-02-11 15:01:59 -0500

    Update 25 packages

---
+++ elpa/haml-mode-20170208.28/haml-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "haml-mode" "20170208.28" "Major mode for editing Haml files" '((ruby-mode "1.0")) :stars '(#("128" 0 3 (font-lock-face paradox-star-face))) :url "https://github.com/nex3/haml-mode" :keywords '("markup" "language" "html"))
