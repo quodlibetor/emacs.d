460b7f7cfdb14e53573ca65a38f6e178fa051732
Brandon W Maister <quodlibetor@gmail.com>
2018-07-08 16:53:31 -0400

    Upgrade things, and add blacken-mode

--- elpa/git-commit-20180329.1311/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20180329.1311" "Edit Git commit messages" '((emacs "24.4") (dash "20170810") (with-editor "20180318")) :stars '(#("2856" 0 4 (font-lock-face paradox-star-face))) :commit "ee925b440e7c7777c90882284647e52a4aea0799" :keywords '("git" "tools" "vc") :url "https://github.com/magit/magit")
