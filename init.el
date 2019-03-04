07f7315dc5c4ca20e90ebc7005bb1f1e74cf541b
Brandon W Maister <quodlibetor@gmail.com>
2019-03-04 15:15:20 -0500

    generic update of init.el

--- init.el
+++ init.el
@@ -87,19 +87,23 @@
  '(ansi-color-names-vector
    (vector "#c5c8c6" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#8abeb7" "#373b41"))
  '(custom-safe-themes
-   '("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa"))
+   (quote
+    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa")))
  '(dired-dwim-target t)
  '(fci-rule-color "#373b41")
- '(magit-log-arguments '("--graph" "--color" "--decorate" "-n256"))
- '(magit-pull-arguments '"--rebase")
- '(magit-tag-arguments '("--annotate"))
+ '(flycheck-yamllintrc ".yamllintrc.yml")
+ '(magit-log-arguments (quote ("--graph" "--color" "--decorate" "-n256")))
+ '(magit-pull-arguments (quote "--rebase"))
+ '(magit-tag-arguments (quote ("--annotate")))
  '(markdown-command "cmark")
- '(org-agenda-files '("~/org/work.org"))
+ '(org-agenda-files (quote ("~/org/work.org")))
  '(package-selected-packages
-   '(flycheck-yamllint blacken company-go yapfify powerline rainbow-mode plantuml-mode smart-mode-line visual-fill-column elpy mmm-mode polymode hydra helm-flycheck scratch avy expand-region multiple-cursors virtualenvwrapper ansible-doc zencoding-mode yasnippet yaml-mode yagist xml-rpc web-mode utop tuareg toml-mode scala-mode2 rust-mode rainbow-delimiters py-gnitset puppet-mode paredit paradox org-plus-contrib nose monky merlin markdown-mode lua-mode json-mode jedi ibuffer-tramp httpcode htmlize highlight-escape-sequences highlight helm-swoop helm-projectile helm-git-grep haskell-mode haml-mode go-mode gitconfig-mode git-rebase-mode git-commit-mode flycheck-rust f dockerfile-mode diminish crontab-mode color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized ack-and-a-half))
+   (quote
+    (py-isort company-go yapfify powerline rainbow-mode plantuml-mode smart-mode-line visual-fill-column elpy mmm-mode polymode hydra helm-flycheck scratch avy expand-region multiple-cursors virtualenvwrapper ansible-doc zencoding-mode yasnippet yaml-mode yagist xml-rpc web-mode utop tuareg toml-mode scala-mode2 rust-mode rainbow-delimiters py-gnitset puppet-mode paredit paradox org-plus-contrib nose monky merlin markdown-mode lua-mode json-mode jedi ibuffer-tramp httpcode htmlize highlight-escape-sequences highlight helm-swoop helm-projectile helm-git-grep haskell-mode haml-mode go-mode gitconfig-mode git-rebase-mode git-commit-mode flycheck-rust f dockerfile-mode diminish crontab-mode color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized ack-and-a-half)))
  '(paradox-automatically-star nil)
  '(safe-local-variable-values
-   '((flycheck-checker . python-flake8-chain)
+   (quote
+    ((flycheck-checker . python-flake8-chain)
      (venv-name . "consumer")
      (venv-current-dir . "/Users/bwm/.virtualenvs/consumer")
      (flycheck-python-mypy-args "--strict-optional" "--ignore-missing-imports" "--fast-parser")
@@ -119,25 +123,29 @@
                             (buffer-file-name)))
            (emacs-lisp-mode)
            (when
-               (fboundp 'flycheck-mode)
+               (fboundp
+                (quote flycheck-mode))
              (flycheck-mode -1))
            (unless
-               (featurep 'package-build)
+               (featurep
+                (quote package-build))
              (let
                  ((load-path
                    (cons ".." load-path)))
-               (require 'package-build)))
+               (require
+                (quote package-build))))
            (package-build-minor-mode))
      (pytest-compile-runner-format . nose)
      (pytest-compile-runner-format quote nose)
      (virtualenv-default-directory . "/home/bwm/projects/schedaddle/")
      (virtualenv-workon . "schedaddle3")
      (pytest-compile-test-runner . "nosetests")
-     (pytest-compile-runner-format . "nose")))
+     (pytest-compile-runner-format . "nose"))))
  '(term-buffer-maximum-size 50000)
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
-   '((20 . "#cc6666")
+   (quote
+    ((20 . "#cc6666")
      (40 . "#de935f")
      (60 . "#f0c674")
      (80 . "#b5bd68")
@@ -154,7 +162,7 @@
      (300 . "#cc6666")
      (320 . "#de935f")
      (340 . "#f0c674")
-     (360 . "#b5bd68")))
+     (360 . "#b5bd68"))))
  '(vc-annotate-very-old-color nil))
  '(dired-dwim-target t)
  '(dired-omit-files "^\\.?#\\|^\\.")
