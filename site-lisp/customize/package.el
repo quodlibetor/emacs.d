f23f8d1522001c624c678228a07ef37196b21395
Brandon W Maister <bwm@materialize.io>
2019-09-13 13:19:40 -0400

    Remove gnu elpa from package archives

--- site-lisp/customize/package.el
+++ site-lisp/customize/package.el
@@ -2,9 +2,6 @@
 
 (setq package-enable-at-startup nil)
 
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives
-             '("org" . "http://orgmode.org/elpa/") t)
-(add-to-list 'package-archives
-             '("elpy" . "http://jorgenschaefer.github.io/packages/"))
+(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("org" . "http://orgmode.org/elpa/")
+                         ("elpy" . "http://jorgenschaefer.github.io/packages/")))
