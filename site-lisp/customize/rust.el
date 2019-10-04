952575683597177d3e2128a56c854ddff73054d5
Brandon W Maister <quodlibetor@gmail.com>
2019-10-04 14:30:43 -0400

    Switch from rust-mode to rustic-mode

--- site-lisp/customize/rust.el
+++ site-lisp/customize/rust.el
@@ -1,4 +1,4 @@
-(require 'rust-mode)
+;; (require 'rust-mode)
 ;; (require 'racer)
 
 ;; (define-key rust-mode-map (kbd "C-j") 'bwm:sane-newline)
@@ -8,32 +8,36 @@
 ;; (add-hook 'rust-mode-hook #'racer-mode)
 ;; (add-hook 'racer-mode-hook #'eldoc-mode)
 ;; (add-hook 'racer-mode-hook #'company-mode)
-(setq rust-format-on-save t)
-(add-hook 'rust-mode-hook
-          (lambda ()
-            ;; (setq-local company-minimum-prefix-length 0)
-            ;; (setq-local compile-command "cargo check")
-                                        ;(cargo-minor-mode 1)
-                                        ;(flycheck-mode)
-            (lsp)))
+;; (setq rust-format-on-save t)
+;; (add-hook 'rust-mode-hook
+;;           (lambda ()
+;;             ;; (setq-local company-minimum-prefix-length 0)
+;;             ;; (setq-local compile-command "cargo check")
+;;                                         ;(cargo-minor-mode 1)
+;;                                         ;(flycheck-mode)
+;;             (lsp)))
 
-(setenv "RUSTC_WRAPPER" "/Users/bwm/.cargo/bin/sccache")
+;;(setenv "RUSTC_WRAPPER" "/Users/bwm/.cargo/bin/sccache")
 
 ;; (add-hook 'before-save-hook
 ;;           (lambda ()
 ;;             ("rustup run nightly rustfmt")))
 ;; (add-hook 'rust-mode-hook #'rustfmt-enable-on-save)
 ;(setq racer-rust-src-path (concat (getenv "HOME") "/src/rust/src"))
-(setq racer-cmd (concat (getenv "HOME") "/.cargo/bin/racer"))
-(setq racer-rust-src-path
-      (concat (getenv "HOME")
-              "/.rustup/toolchains/stable-x86_64-apple-darwin/lib/rustlib/src/rust/src"))
-(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
-(setq company-tooltip-align-annotations t)
+;; (setq racer-cmd (concat (getenv "HOME") "/.cargo/bin/racer"))
+;; (setq racer-rust-src-path
+;;       (concat (getenv "HOME")
+;;               "/.rustup/toolchains/stable-x86_64-apple-darwin/lib/rustlib/src/rust/src"))
+;; (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+;; (setq company-tooltip-align-annotations t)
 
-;; override the cargo error message face to draw more attention to parts of the
-;; buffer used for testing
-(require 'compile)
-(add-to-list 'compilation-error-regexp-alist-alist
-             '(cargo "thread '\\([^']+\\)' panicked at \\('[^']+'\\), \\([^:]+\\):\\([0-9]+\\)"
-                     3 4 nil nil 2 (1 'compilation-info)))
+;; ;; override the cargo error message face to draw more attention to parts of the
+;; ;; buffer used for testing
+;; (require 'compile)
+;; (add-to-list 'compilation-error-regexp-alist-alist
+;;              '(cargo "thread '\\([^']+\\)' panicked at \\('[^']+'\\), \\([^:]+\\):\\([0-9]+\\)"
+;;                     3 4 nil nil 2 (1 'compilation-info)))
+
+(use-package rustic
+  :init
+  (setq rustic-format-trigger 'on-save))
