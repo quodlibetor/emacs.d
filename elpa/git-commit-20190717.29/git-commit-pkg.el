90b0bd420c70ab7493a3748d5674a671c59d6fad
Brandon W Maister <quodlibetor@gmail.com>
2019-10-04 14:05:51 -0400

    Update 26 packages, delete 2

--- elpa/git-commit-20190717.29/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20190717.29" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "9e2df13e5338b7bde31dceca4e64494bc0fd68a8" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
