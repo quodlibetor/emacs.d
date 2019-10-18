4774fc1b1d53dc9290a1c7c51de8c19c0104878e
Brandon W Maister <quodlibetor@gmail.com>
2019-10-18 11:16:34 -0400

    trust elpa.gnu.org (again) and melpa-stable
    elpa is required for clojure/cider modes, even though I get tls errors when
    trying to load it.

--- site-lisp/customize/package.el
+++ site-lisp/customize/package.el
@@ -3,5 +3,7 @@
 (setq package-enable-at-startup nil)
 
 (setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("melpa-stable" . "http://stable.melpa.org/packages/")
                          ("org" . "http://orgmode.org/elpa/")
-                         ("elpy" . "http://jorgenschaefer.github.io/packages/")))
+                         ("elpy" . "http://jorgenschaefer.github.io/packages/")
+                         ("gnu" . "https://elpa.gnu.org/packages/")))
