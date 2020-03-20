253ed76144a33854815fab285b143b1a1600bb46
Brandon W Maister <quodlibetor@gmail.com>
2020-03-20 10:32:35 -0400

    package.el: commit to security

--- site-lisp/customize/package.el
+++ site-lisp/customize/package.el
@@ -2,8 +2,8 @@
 
 (setq package-enable-at-startup nil)
 
-(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
-                         ("melpa-stable" . "http://stable.melpa.org/packages/")
+(setq package-archives '(("melpa" . "https://melpa.milkbox.net/packages/")
+                         ("melpa-stable" . "https://stable.melpa.org/packages/")
                          ("org" . "http://orgmode.org/elpa/")
                          ("elpy" . "http://jorgenschaefer.github.io/packages/")
                          ("gnu" . "https://elpa.gnu.org/packages/")))
