a6298a95723cd18b9d6e7f21a80a8358f9af8e13
Brandon W Maister <quodlibetor@gmail.com>
2021-02-18 10:09:14 -0500

    Install dap-mode
    from https://robert.kra.hn/posts/2021-02-07_rust-with-emacs/

--- site-lisp/customize/rust.el
+++ site-lisp/customize/rust.el
@@ -68,3 +68,25 @@
           (lambda ()
             (when (eq major-mode 'rustic-mode)
               (lsp-format-buffer))))
+
+
+;; Debugging
+
+(use-package dap-mode
+  :ensure
+  :config
+  (dap-ui-mode)
+  (dap-ui-controls-mode 1)
+
+  (require 'dap-lldb)
+  (require 'dap-gdb-lldb)
+  ;; installs .extension/vscode
+  (dap-gdb-lldb-setup)
+  (dap-register-debug-template
+   "Rust::LLDB Run Configuration"
+   (list :type "lldb"
+         :request "launch"
+         :name "LLDB::Run"
+	 :gdbpath "rust-lldb"
+         :target nil
+         :cwd nil)))
