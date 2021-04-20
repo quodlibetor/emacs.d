1fbee2bbd065951742d5756d8ef3049300a0b93c
Brandon W Maister <quodlibetor@gmail.com>
2021-04-20 15:52:01 -0400

    Update rust config

--- site-lisp/customize/rust.el
+++ site-lisp/customize/rust.el
@@ -43,6 +43,49 @@
       (lsp-find-definition)
     (lsp-ui-peek-find-definitions)))
 
+;; Speed up rust compilation, using the same flags as elsewhere
+(setenv "CARGO_PROFILE_DEV_DEBUG" "1")
+(setenv "RUSTFLAGS" " -Csplit-debuginfo=unpacked -Clink-arg=-fuse-ld=lld")
+(setenv "PATH" (concat (getenv "HOME") "/.local/clang/bin" ":"
+                       (getenv "PATH")))
+
+;; Debugging
+
+;; troubleshooting in https://github.com/emacs-lsp/dap-mode/issues/try The
+;; command defined by `dap-gdb-lldb-debug-program' should work I needed to
+;; manually download
+;; https://marketplace.visualstudio.com/items?itemName=webfreak.debug copy it into
+;; ~/.emacs.d/.extension/vscode/webfreak.debug/ and unzip it
+
+(use-package dap-mode
+  :ensure
+  :config
+  (dap-auto-configure-mode)
+
+  (require 'dap-lldb)
+  (require 'dap-gdb-lldb)
+  ;; installs .extension/vscode
+  (dap-gdb-lldb-setup)
+  (setenv "MZ_DEV" "1")
+  (dap-register-debug-template
+   "Rust::LLDB Run Configuration"
+   (list :type "lldb"
+         :request "launch"
+         :name "LLDB::Run"
+	 :gdbpath "rust-lldb"
+         :target nil
+         :cwd nil))
+  (dap-register-debug-template
+   "materialize"
+   (list :type "lldb"
+         :request "launch"
+         :name "LLDB::Run"
+	 :gdbpath "rust-lldb"
+         :target nil
+         :cwd "/Users/bwm/github/materialize"))
+  (add-hook 'dap-stopped-hook
+            (lambda (arg) (call-interactively #'dap-hydra))))
+
 (use-package rust-mode)
 (use-package rustic
   :hook (rust-mode . rustic-mode)
@@ -52,41 +95,20 @@
               ("M-." . bwm:rust-find-definition)
               ("C-." . lsp-ui-peek-find-references)
               ("<C-return>" . helm-lsp-code-actions)
-              ("C-h f" . lsp-ui-doc-show))
+              ("C-h f" . lsp-ui-doc-show)
+              ("C-c C-c d" . dap-hydra))
   :custom
   (rustic-format-trigger nil "use lsp format")
   (rustic-lsp-format t "use the lsp server instead of cargo fmt")
   (lsp-ui-doc-show-with-cursor nil "Use `C-h f' for a doc view"))
-
-;; one of the two of these should be enabled
-(setq lsp-rust-analyzer-proc-macro-enable t)
-(setq lsp-rust-analyzer-import-merge-behaviour "last")
-(setq lsp-rust-analyzer-cargo-load-out-dirs-from-check t)
-; (setq lsp-rust-analyzer-diagnostics-disabled '("unresolved-proc-macro"))
 
 (add-hook 'before-save-hook
           (lambda ()
             (when (eq major-mode 'rustic-mode)
               (lsp-format-buffer))))
 
-
-;; Debugging
-
-(use-package dap-mode
-  :ensure
-  :config
-  (dap-ui-mode)
-  (dap-ui-controls-mode 1)
-
-  (require 'dap-lldb)
-  (require 'dap-gdb-lldb)
-  ;; installs .extension/vscode
-  (dap-gdb-lldb-setup)
-  (dap-register-debug-template
-   "Rust::LLDB Run Configuration"
-   (list :type "lldb"
-         :request "launch"
-         :name "LLDB::Run"
-	 :gdbpath "rust-lldb"
-         :target nil
-         :cwd nil)))
+;; one of the two of these should be enabled
+(setq lsp-rust-analyzer-proc-macro-enable t)  ; get access to proc macros, will eventually be on by default
+(setq lsp-rust-analyzer-import-merge-behaviour "last") ; materialize import style
+(setq lsp-rust-analyzer-cargo-load-out-dirs-from-check t) ; ensure coordinated compilation dirs
+; (setq lsp-rust-analyzer-diagnostics-disabled '("unresolved-proc-macro"))
