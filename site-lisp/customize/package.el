23a42143011d3f8d722dff719e2f2b9deedc79be
Brandon W Maister <quodlibetor@gmail.com>
2021-07-14 16:38:56 -0400

    Add some useful shortcuts to list-packages buffer

--- site-lisp/customize/package.el
+++ site-lisp/customize/package.el
@@ -8,3 +8,19 @@
                          ("elpy" . "http://jorgenschaefer.github.io/packages/")
                          ;("gnu" . "https://elpa.gnu.org/packages/")
                          ))
+
+(defun package-menu-find-marks ()
+  "Find packages marked for action in *Packages*."
+  (interactive)
+  (occur "^[A-Z]"))
+
+;; Only in Emacs 25.1+
+(defun package-menu-filter-by-status (status)
+  "Filter the *Packages* buffer by status."
+  (interactive
+   (list (completing-read
+          "Status: " '("new" "installed" "dependency" "obsolete"))))
+  (package-menu-filter-by-keyword (concat "status:" status)))
+
+(define-key package-menu-mode-map "s" #'package-menu-filter-by-status)
+(define-key package-menu-mode-map "a" #'package-menu-find-marks)
