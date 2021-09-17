6071c84f4677ff66b25ab8b9a485e489d20d5560
Brandon W Maister <quodlibetor@gmail.com>
2021-09-17 12:25:10 -0400

    Update 39 packages

--- elpa/helm-core-20210714.1600/helm-core-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "helm-core" "20210714.1600" "Development files for Helm"
-  '((emacs "25.1")
-    (async "1.9.4"))
-  :commit "60db46905443c87f324440202bd0735d57c5c201")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/helm-core-20210914.749/helm-core-pkg.el
@@ -0,0 +1,7 @@
+(define-package "helm-core" "20210914.749" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.4"))
+  :commit "996e4c63532b16e797740f8fe529d79521e9327f")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
