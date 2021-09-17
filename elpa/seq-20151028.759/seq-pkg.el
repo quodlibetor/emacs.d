ac1cd2ec0ee59bf00444b5a7a8025109e7c1e406
Brandon W Maister <quodlibetor@gmail.com>
2021-09-17 12:16:48 -0400

    remove unused package

--- elpa/seq-20151028.759/seq-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "seq" "20151028.759" "Sequence manipulation functions" 'nil :stars '(#("102" 0 3 (font-lock-face paradox-starred-face))) :keywords '("sequences"))
