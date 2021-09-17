57586027fb5b15bb047ec9d01d37383a8614dec4
Brandon W Maister <quodlibetor@gmail.com>
2021-09-17 12:17:15 -0400

    Highlight TODO(note): comments

--- site-lisp/customize/prog.el
+++ site-lisp/customize/prog.el
@@ -1,6 +1,6 @@
 (defun add-todo-keywords ()
   (font-lock-add-keywords nil
-                          '(("\\<\\(FIXME\\|TODO\\|BUG\\|XXX\\):" 1 font-lock-warning-face t))))
+                          '(("\\<\\(FIXME\\|TODO\\|BUG\\|XXX\\)\\(([^)]+)\\)?:" 1 font-lock-warning-face t))))
 
 (add-hook 'yaml-mode-hook
           #'add-todo-keywords)
