e6d64ab813a1e34f071fc7f78e5953d71629e340
Brandon W Maister <quodlibetor@gmail.com>
2021-11-04 11:22:36 -0400

    update 15 packages

--- elpa/protobuf-mode-20200619.1742/protobuf-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "protobuf-mode" "20200619.1742" "major mode for editing protocol buffers." 'nil :commit "86b3ccf28ca437330cc42a2b3a75a1314977fcfd" :keywords '("google" "protobuf" "languages") :authors '(("Alexandre Vassalotti" . "alexandre@peadrop.com")) :maintainer '("Alexandre Vassalotti" . "alexandre@peadrop.com"))
