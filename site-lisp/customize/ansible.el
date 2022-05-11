bc13bd999c32f66ab730c36ff1a43487f67f8cc0
Brandon W Maister <quodlibetor@gmail.com>
2022-05-11 10:27:02 -0400

    Highlight jinja2 sections in ansible

--- site-lisp/customize/ansible.el
+++ site-lisp/customize/ansible.el
@@ -4,3 +4,6 @@
 (use-package ansible-doc
   :straight t
   :hook (ansible-mode . ansible-doc-mode))
+
+(use-package jinja2-minor-mode
+  :straight (jinja2-minor-mode :type git :host github :repo "krig/jinja2-minor-mode"))
