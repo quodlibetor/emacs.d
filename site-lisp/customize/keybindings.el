b3a8531127b970df64a397df6163e42ce91f7daa
Brandon W Maister <quodlibetor@gmail.com>
2022-05-11 10:29:24 -0400

    find-file-at-point > find-file

--- site-lisp/customize/keybindings.el
+++ site-lisp/customize/keybindings.el
@@ -36,6 +36,7 @@
 (global-set-key (kbd "C-h a") 'helm-apropos)
 (global-set-key (kbd "C-z") 'repeat)
 (global-set-key (kbd "C-c ! h") 'helm-flycheck)
+(global-set-key (kbd "C-x C-f") 'ffap)
 (global-set-key (kbd "C-x C-j") 'dired-jump)
 
 
